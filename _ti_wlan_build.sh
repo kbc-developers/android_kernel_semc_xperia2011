02477d4b0b7330854f5082048f50a920bead9a8a
sakuramilk <c.sakuramilk@gmail.com>
2012-10-17 13:29:56 +0900

    build: added cifs, ntfs, fuse support

--- _ti_wlan_build.sh
+++ _ti_wlan_build.sh
@@ -24,12 +24,12 @@ else
         rm ./drivers/net/wireless/wl1251/wl1251_sdio.ko
     fi
 
-    cp -av ./compat/compat.ko $MODULE_DIR
-    cp -av ./compat/compat_firmware_class.ko $MODULE_DIR
-    cp -av ./drivers/net/wireless/wl12xx/wl12xx.ko $MODULE_DIR
-    cp -av ./drivers/net/wireless/wl12xx/wl12xx_sdio.ko $MODULE_DIR
-    cp -av ./net/mac80211/mac80211.ko $MODULE_DIR
-    cp -av ./net/wireless/cfg80211.ko $MODULE_DIR
+    #cp -av ./compat/compat.ko $MODULE_DIR
+    #cp -av ./compat/compat_firmware_class.ko $MODULE_DIR
+    #cp -av ./drivers/net/wireless/wl12xx/wl12xx.ko $MODULE_DIR
+    #cp -av ./drivers/net/wireless/wl12xx/wl12xx_sdio.ko $MODULE_DIR
+    #cp -av ./net/mac80211/mac80211.ko $MODULE_DIR
+    #cp -av ./net/wireless/cfg80211.ko $MODULE_DIR
 fi
 
 cd $TOP_DIR
